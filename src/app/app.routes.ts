a9f2c462b7f53e3fa01561722e87bec7e54d6ff2
f3lip3mp <pmenezesfelipe@gmail.com>
2024-12-05 17:56:32 -0300

    feat: configura rotas medicamentos e usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MedicamentosComponent } from './components/medicamentos/medicamentos.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'medicamentos', component: MedicamentosComponent },
+  { path: 'usuarios', component: UsuariosComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

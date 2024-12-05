3981ad058733bdecde91336baeebe9be744f3501
f3lip3mp <pmenezesfelipe@gmail.com>
2024-12-05 18:00:34 -0300

    feat: crud de pesquisa - adicao - remocao

--- src/app/services/medicamento.service.ts
+++ src/app/services/medicamento.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MedicamentoService {
+  private apiUrl = 'http://localhost:3000';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getMedicamentos(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/medicamentos`);
+  }
+
+  addMedicamento(medicamento: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/medicamentos`, medicamento);
+  }
+
+  deleteMedicamento(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/medicamentos/${id}`);
+  }
 }

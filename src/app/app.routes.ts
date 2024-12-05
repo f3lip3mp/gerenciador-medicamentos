c8cbb95c0a935bdef09b570831d2691f2d005d43
f3lip3mp <pmenezesfelipe@gmail.com>
2024-12-05 18:28:38 -0300

    feat: ajusta a tela inicial do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,9 @@ import { HomeComponent } from './components/home/home.component';
 import { MedicamentosComponent } from './components/medicamentos/medicamentos.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
-export const routes: Routes = [
+export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'medicamentos', component: MedicamentosComponent },
   { path: 'usuarios', component: UsuariosComponent },
+  { path: '**', redirectTo: '' },
 ];

c6ee2ec2d7ade51ee213220583a2487e68730db8
f3lip3mp <pmenezesfelipe@gmail.com>
2024-12-12 15:13:02 -0300

    feat: chamada para api json

--- src/app/services/medicamento.service.ts
+++ src/app/services/medicamento.service.ts
@@ -6,19 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class MedicamentoService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'http://localhost:3000/medicamentos'; // Endpoint do JSON Server
 
   constructor(private http: HttpClient) {}
 
   getMedicamentos(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/medicamentos`);
-  }
-
-  addMedicamento(medicamento: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/medicamentos`, medicamento);
-  }
-
-  deleteMedicamento(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/medicamentos/${id}`);
+    return this.http.get<any[]>(this.apiUrl);
   }
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsuarioService {
+  private apiUrl = 'http://localhost:3000/usuarios'; // Endpoint do JSON Server
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getUsuarios(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
